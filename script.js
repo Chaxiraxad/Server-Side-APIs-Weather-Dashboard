1d350c9d20ab556700036089567183a042be49a4
chaxiraxad <chaxiraxad@gmail.com>
2023-01-30 18:09:20 +0000

    add files

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const apiKey = "1cf52c354cfb0d79b21f85abef3fc8a9";
 const history = JSON.parse(localStorage.getItem('history')) || [];
 
 
-$('#search-form').on('submit', function (event) {
+$('#search').on('submit', function (event) {
     event.preventDefault();
-    const userInput = $('#search-input').val();
+    const userInput = $('#city-input').val();
     const queryUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=5&appid=' + apiKey;
 
 
@@ -21,6 +21,7 @@ $('#search-form').on('submit', function (event) {
             console.log(response)
             const lat = response[0].lat;
             const lon = response[0].lon;
+      
 
             const weatherQueryUrl = 'http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
 
@@ -28,17 +29,24 @@ $('#search-form').on('submit', function (event) {
             $.ajax({ url: weatherQueryUrl })
                 .then(function (weatherResponse) {
                     // Icon URL http://openweathermap.org/img/w/" + iconcode + ".png"
-
+console.log(weatherResponse)
                     // Put the response on the HTML page
                     const weatherList = weatherResponse.list;
                     // Now forecast
                     const today = weatherList[0];
                     console.log(today);
+                    var currentTemp = today.main.temp
+                    var currentHumidity = today.main.humidity
+                    var currentWind = today.wind.speed
+$("#temp").text("Temperature:" + currentTemp)
                     // TODO: put today's weather in container for today's weather
-
+var counts = 0
                     // 5 days forecast
                     for (let i = 1; i < weatherList.length; i += 8) {
                         const weather = weatherList[i];
+                        var weatherTemp = weather.main.temp
+                        counts ++
+                        $("#temp" + counts).text("Temperature:" + weatherTemp)        
                         console.log(weather);
                         // TODO: put 5 day's forecast weather in container for the 5 day forecast
                     }
