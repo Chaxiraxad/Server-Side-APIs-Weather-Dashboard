97c4660af94aa16bfd66523d86af823a4ac7d11b
chaxiraxad <chaxiraxad@gmail.com>
2023-01-31 19:17:52 +0000

    added 5 days icons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiKey = "1cf52c354cfb0d79b21f85abef3fc8a9";
 const history = JSON.parse(localStorage.getItem('history')) || [];
-var city 
+
 
 
 $('#search').on('submit', function (event) {
@@ -85,42 +85,50 @@ $('#search').on('submit', function (event) {
                         $("#wind" + counts).text("Wind speed: " + weatherWind + "mph")
                         $("#descrip" + counts).text("Description: " + weatherDescrip)
                         // $("#time-date").text(currentTime)
+                        $("#icon" + counts).html(
+                            `<img src="https://openweathermap.org/img/wn/${weatherIcon}.png">`
+                        );
+
                         console.log(weather);
+
+
+
+                  
                         // TODO: put 5 day's forecast weather in container for the 5 day forecast
                     }
 
-                    var day1 = moment().add(1, "days").format("D MMMM YYYY")
-                    var day2 = moment().add(2, "days").format("D MMMM YYYY")
-                    var day3 = moment().add(3, "days").format("D MMMM YYYY")
-                    var day4 = moment().add(4, "days").format("D MMMM YYYY")
-                    var day5 = moment().add(5, "days").format("D MMMM YYYY")
-                    $("#date1").text(day1);
-                    $("#date2").text(day2);
-                    $("#date3").text(day3);
-                    $("#date4").text(day4);
-                    $("#date5").text(day5);
+                    // var day1 = moment().add(1, "days").format("D MMMM YYYY")
+                    // var day2 = moment().add(2, "days").format("D MMMM YYYY")
+                    // var day3 = moment().add(3, "days").format("D MMMM YYYY")
+                    // var day4 = moment().add(4, "days").format("D MMMM YYYY")
+                    // var day5 = moment().add(5, "days").format("D MMMM YYYY")
+                    // $("#date1").text(day1);
+                    // $("#date2").text(day2);
+                    // $("#date3").text(day3);
+                    // $("#date4").text(day4);
+                    // $("#date5").text(day5);
 
-                    var icon1 = weather.weather[0].icon;
-                    var icon2 = weather.weather[0].icon;
-                    var icon3 = weather.weather[0].icon;
-                    var icon4 = weather.weather[0].icon;
-                    var icon5 = weather.weather[0].icon;
+                    // var icon1 = weather.weather[0].icon;
+                    // var icon2 = weather.weather[0].icon;
+                    // var icon3 = weather.weather[0].icon;
+                    // var icon4 = weather.weather[0].icon;
+                    // var icon5 = weather.weather[0].icon;
 
-                    $("#icon1").html(
-                        `<img src="http://openweathermap.org/img/wn/${icon1}@2x.png">`
-                      );
-                      $("#icon2").html(
-                        `<img src="http://openweathermap.org/img/wn/${icon2}@2x.png">`
-                      );
-                      $("#icon3").html(
-                        `<img src="http://openweathermap.org/img/wn/${icon3}@2x.png">`
-                      );
-                      $("#icon4").html(
-                        `<img src="http://openweathermap.org/img/wn/${icon4}@2x.png">`
-                      );
-                      $("#icon5").html(
-                        `<img src="http://openweathermap.org/img/wn/${icon5}@2x.png">`
-                      );
+                    // $("#icon1").html(
+                    //     `<img src="http://openweathermap.org/img/wn/${icon1}@2x.png">`
+                    //   );
+                    //   $("#icon2").html(
+                    //     `<img src="http://openweathermap.org/img/wn/${icon2}@2x.png">`
+                    //   );
+                    //   $("#icon3").html(
+                    //     `<img src="http://openweathermap.org/img/wn/${icon3}@2x.png">`
+                    //   );
+                    //   $("#icon4").html(
+                    //     `<img src="http://openweathermap.org/img/wn/${icon4}@2x.png">`
+                    //   );
+                    //   $("#icon5").html(
+                    //     `<img src="http://openweathermap.org/img/wn/${icon5}@2x.png">`
+                    //   );
                 });
         });
 });
