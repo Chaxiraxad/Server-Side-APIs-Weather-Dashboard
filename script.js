5d88d9768e16b0af1f02198a9c685caabc3b825a
chaxiraxad <chaxiraxad@gmail.com>
2023-02-27 20:55:14 +0000

    this commit  click on a city in the search history they are again presentwith the data and adds weather from Tenerife when the page starts

--- script.js
+++ script.js
@@ -131,7 +131,9 @@ $("#clr-btn").on('click', function (event) {
 $("#cityList").on('click', function (event) {
 console.log(event)
 // Finish the line below to grab the text of the button that was clicked
-const userInput =  event
+const userInput =  event.target.innerText
 // $("#cityList").text(event)
 getData(userInput)
 })
+
+getData('Tenerife')
